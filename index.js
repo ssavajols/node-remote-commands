fa6731b229a33c2c32354a0c7724354f373a40dc
Sylvain Savajols <s.savajols@gmail.com>
2018-03-14 09:07:49 +0100

    updates

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const child = require('child_process').spawn
+const nunjucks = require('nunjucks')
+let commads = loadCommands()
+
+const app = express()
+
+app.get('/', (req, res) => {
+
+  res.send(nunjucks.render('index.html', {items: commands}))
+})
+
+app.get('/run/:command', (req, res) => {
+
+  console.log(req.params.command, commands, commands[req.params.command])
+
+  const p = child(commands[req.params.command].command, [], {
+    cwd: commands[req.params.command].cwd || process.cwd(),
+    shell: true
+  })
+
+  p.stdout.on('data', (data) => {
+    res.send(data.toString());
+  });
+  
+  p.stderr.on('data', (data) => {
+    res.send(data.toString());
+  });
+  
+  p.on('exit', (code) => {
+    console.log(`Child exited with code ${code}`);
+  });
+
+})
+
+app.listen(5001, () => {
+  console.log('listening 5001')
+})
+
+function loadCommands() {
+  return commands = require('./commands.json')
+}

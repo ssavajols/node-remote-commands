da5acd26497e210b974dda529de24eb2d0ac1135
Sylvain Savajols <s.savajols@gmail.com>
2018-03-14 19:32:32 +0100

    update

--- index.js
+++ index.js
@@ -4,29 +4,55 @@ const nunjucks = require('nunjucks')
 let commads = loadCommands()
 
 const app = express()
+const commandRunning = {}
 
 app.get('/', (req, res) => {
-
   res.send(nunjucks.render('index.html', {items: commands}))
+})
+
+app.get('/log/:command', (req, res) => {
+  const cmd = commandRunning[req.params.command]
+  
+  if ( cmd && cmd.log ) {
+    res.send(nunjucks.render('log.html', {log: cmd.log}))
+    return
+  }
+  res.send(req.params.command+' : not running')
 })
 
 app.get('/run/:command', (req, res) => {
 
+  let cmd = commandRunning[req.params.command]
+
+  res.redirect('/log/'+req.params.command)
+
+  if( cmd && cmd.isRunning ) {
+    return 
+  }
+
   const p = child(commands[req.params.command].command, [], {
     cwd: commands[req.params.command].cwd || process.cwd(),
     shell: true
+  }, () => {
+    cmd.isRunning = false
   })
 
+  cmd = commandRunning[req.params.command] = {
+    log: "running at " + (new Date).toString() + "<br />",
+    isRunning: true
+  }
+
   p.stdout.on('data', (data) => {
-    res.write(data.toString());
+    cmd.log += data.toString() + "<br />"
   });
   
   p.stderr.on('data', (data) => {
-    res.write(data.toString());
+    cmd.log += data.toString() + "<br />"
   });
   
   p.on('exit', (code) => {
-    res.end(`Child exited with code ${code}`);
+    cmd.isRunning = false
+    cmd.log += "finished => " + code + "<br />"
   });
 
 })
